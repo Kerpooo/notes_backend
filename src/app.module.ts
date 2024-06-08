f93577c10734c60835382acb7c128a84f19342c6
Kerpooo <kevinrincon8@gmail.com>
2024-06-08 13:42:09 -0500

    feat: :construction: Add Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 import { NotesModule } from './notes/notes.module';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [NotesModule, CategoriesModule],
+  imports: [PrismaModule, NotesModule, CategoriesModule],
 
 })
 export class AppModule { }
